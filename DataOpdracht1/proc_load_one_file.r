8dad2b18c85ee95eba131ae56a20a61f3a6120e7
Dylan Driessen <dylan.driessen@student.ucll.be>
2019-02-15 15:00:50 +0100

    parallel benchmark

--- DataOpdracht1/proc_load_one_file.r
+++ DataOpdracht1/proc_load_one_file.r
@@ -5,18 +5,27 @@ ps18b_abstr_cln <- PS18B_ABSTR_01
 
 
 no_cores <- detectCores()-1
-cl <- makeCluster(no_cores)
-parApply(cl, PS18B_ABSTR_01$appln_abstract, function(i){
-  #stringi::stri_trans_general(i, 'Latin-ASCII')
-  i
-})
+
+parLapplyFunc <- function() {
+  cl <- makeCluster(no_cores)
+  ps18b_abstr_cln$appln_abstract <- parLapply(cl, PS18B_ABSTR_01$appln_abstract, stringi::stri_trans_general, 'Latin-ASCII')
+  stopCluster(cl)
+}
+
+
+
+parApplyFunc <- function() {
+  cl <- makeCluster(no_cores)
+  ps18b_abstr_cln$appln_abstract <- parApply(cl, as.array(PS18B_ABSTR_01$appln_abstract), 1, stringi::stri_trans_general, 'Latin-ASCII')
+  stopCluster(cl)
+}
 
 library(microbenchmark)
 serial <- function(){
-  ps18b_abstr_cln$appln_abtract <- stringi::stri_trans_general(PS18B_ABSTR_01$appln_abstract, 'Latin-ASCII') 
+  ps18b_abstr_cln$appln_abstract <- stringi::stri_trans_general(PS18B_ABSTR_01$appln_abstract, 'Latin-ASCII') 
 }
 
-micro <- microbenchmark(serial(), times = 3)
+micro <- microbenchmark(serial(), parLapplyFunc(), parApplyFunc(), times = 3)
 micro
 plot(micro)
 
