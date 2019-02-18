5f5e718b3635b954efe6dc279dd55b21a05cb581
Vincent Heeren <vincent.heeren@student.ucll.be>
2019-02-18 14:12:51 +0100

    add clusterApply

--- DataOpdracht1/proc_load_one_file.r
+++ DataOpdracht1/proc_load_one_file.r
@@ -3,29 +3,39 @@ library(parallel)
 
 ps18b_abstr_cln <- PS18B_ABSTR_01
 
-
 no_cores <- detectCores()-1
 
-parLapplyFunc <- function() {
+parLapplyParallel <- function() {
   cl <- makeCluster(no_cores)
   ps18b_abstr_cln$appln_abstract <- parLapply(cl, PS18B_ABSTR_01$appln_abstract, stringi::stri_trans_general, 'Latin-ASCII')
   stopCluster(cl)
 }
 
-
-
-parApplyFunc <- function() {
+parApplyParallel <- function() {
   cl <- makeCluster(no_cores)
   ps18b_abstr_cln$appln_abstract <- parApply(cl, as.array(PS18B_ABSTR_01$appln_abstract), 1, stringi::stri_trans_general, 'Latin-ASCII')
   stopCluster(cl)
 }
 
+clusterApplyParallel <- function() {
+  cl <- makeCluster(no_cores)
+  
+  #put in groups
+  groups <- sample(no_cores, length(PS18B_ABSTR_01$appln_abstract), replace = TRUE)
+  split_groups <- split(PS18B_ABSTR_01$appln_abstract, groups)
+  split_groups
+  
+  res <- clusterApply(cl, split_groups, stringi::stri_trans_general,'Latin-ASCII')
+  ps18b_abstr_cln$appln_abstract <- unlist(res)
+  
+  stopCluster(cl)
+}
+
 library(microbenchmark)
-serial <- function(){
+sequential <- function(){
   ps18b_abstr_cln$appln_abstract <- stringi::stri_trans_general(PS18B_ABSTR_01$appln_abstract, 'Latin-ASCII') 
 }
 
-micro <- microbenchmark(serial(), parLapplyFunc(), parApplyFunc(), times = 3)
+micro <- microbenchmark(sequential(), parLapplyParallel(), parApplyParallel(), clusterApplyParallel(), times = 3)
 micro
 plot(micro)
-
