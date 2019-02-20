5a396f42ca5b8ab3efb8a2af87948cc60dd5c023
Pieter Huybrechts <huybrechtspieter@gmail.com>
2019-02-20 14:33:10 +0100

    Preprocessing parallel

--- DataOpdracht1/lib/preProcess.r
+++ DataOpdracht1/lib/preProcess.r
@@ -4,7 +4,7 @@ preProcess <- function() {
   docs$text <- stringi::stri_trans_general(docs$text, 'Latin-ASCII')
 }
 
-parPreProcess <- function() {
+parLPreProcess <- function() {
   import(c("stringi","parallel"))   # String manipulation
   no_cores = detectCores()-1
   cluster <- makeCluster(no_cores)
@@ -12,4 +12,17 @@ parPreProcess <- function() {
   # TRANSLITERATE TO LATIN
   parLapply(cl,docs$text,stringi::stri_trans_general,id="Latin-ASCII")
   stopCluster(cluster)
+}
+
+fePreProcess <- function() {
+  foreach(str = docs$text) %do%
+    stringi::stri_trans_general(str=str,id="Latin_ASCII")
+}
+
+feParProcess <- function() {
+  import("doParallel")
+  registerDoPar(detectCores()-1)
+  
+  foreach(str = docs$text) %do%
+    stringi::stri_trans_general(str=str,id="Latin_ASCII")
 }
