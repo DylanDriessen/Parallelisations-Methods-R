90d030ba010607ac11e7f4a8e140cc380e6277bd
Pieter Huybrechts <huybrechtspieter@gmail.com>
2019-02-20 14:30:57 +0100

    preprocessing with parLapply

--- DataOpdracht1/lib/preProcess.r
+++ DataOpdracht1/lib/preProcess.r
@@ -3,3 +3,13 @@ preProcess <- function() {
   # TRANSLITERATE TO LATIN
   docs$text <- stringi::stri_trans_general(docs$text, 'Latin-ASCII')
 }
+
+parPreProcess <- function() {
+  import(c("stringi","parallel"))   # String manipulation
+  no_cores = detectCores()-1
+  cluster <- makeCluster(no_cores)
+  
+  # TRANSLITERATE TO LATIN
+  parLapply(cl,docs$text,stringi::stri_trans_general,id="Latin-ASCII")
+  stopCluster(cluster)
+}
