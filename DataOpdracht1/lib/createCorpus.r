6fa9246a58aad96f2e0a8af4ab966efaa8b26c14
DylanDriessen <dylan.driessen@student.ucll.be>
2019-02-21 14:00:36 +0100

    stopWords cleaning

--- DataOpdracht1/lib/createCorpus.r
+++ DataOpdracht1/lib/createCorpus.r
@@ -12,68 +12,125 @@ createCorpus <- function() {
       "doParallel",
       "microbenchmark",
       "text2vec",
-      "doMC"
-      ,"quanteda",
+      "doMC",
+      "quanteda",
       "textmineR"
     )
   )
   
-  # Create corpus (and define default language)
+  ##### Create corpus (and define default language)
   
+  VCorp()
+}
+
+#####################################################################
+##
+## TM Package
+##
+#####################################################################
+
+VCorp <- function() {
   crp <-
     VCorpus(DataframeSource(docs), readerControl = list(language = "en"))
   
-  # Set metadata based on fields in source data
   
-  seqClus <- function() {
-    for (i in 1:length(crp)) {
-      meta(crp[[i]], "language") <- docs$language[i]
-    }
-  }
+  ##### Set metadata based on fields in source data
   
-  parClus <- function() {
-    no_cores <- detectCores()
-    #cl <- makeCluster(no_cores)
-    registerDoMC(no_cores)
-    foreach(i = 1:length(crp), .export = c("docs", "crp"), .packages = c("tm")) %dopar% {
-      meta(crp[[i]], "language") <- docs$language[i]
-    }
-    #stopCluster(cl)
-  }
+  # seqClus <- function() {
+  #   for (i in 1:length(crp2)) {
+  #     metadoc(crp2, i, "language") <- docs$language[i]
+  #   }
+  # }
+  # 
+  # parClus <- function() {
+  #   no_cores <- detectCores()
+  #   #cl <- makeCluster(no_cores)
+  #   registerDoMC(no_cores)
+  #   foreach(
+  #     i = 1:length(crp),
+  #     .export = c("docs", "crp"),
+  #     .packages = c("tm")
+  #   ) %dopar% {
+  #     meta(crp[[i]], "language") <- docs$language[i]
+  #   }
+  #   #stopCluster(cl)
+  # }
   
-  microbenchmark(seqClus(),parClus(), times = 10)
-  
-  # Define general function to replace strings in corpus
+  ##### Define general function to replace strings in corpus
   (crp.replacePattern <-
       content_transformer(function(x, pattern, replace)
         gsub(pattern, replace, x)))
   
-  ## Clean unicode characters
-  
-  # Remove graphical characters
+  ##### Clean unicode characters
+  ##### Remove graphical characters
   crp <- tm_map(crp, crp.replacePattern, "[^[:graph:]]", " ")
   
-  # To lower
+  ##### To lower
   crp <- tm_map(crp, content_transformer(tolower))
   
-  # Stopword removal
-  crp <- tm_map(crp, removeWords, stopwords("SMART"))
+  ##### Stopword removal
+  crp <- tm_map(crp, removeWords, stopwords(source ="smart"))
   
-  # Stemming
+  ##### Stemming
   crp <- tm_map(crp, stemDocument, language = "porter")
   
-  # Numbers
+  ##### Numbers
   
-  # All numbers (including numbers as part of a alphanumerical term)
+  ##### All numbers (including numbers as part of a alphanumerical term)
   crp <- tm_map(crp, removeNumbers)
   
-  # Punctuation
+  ##### Punctuation
   crp <-
     tm_map(crp, removePunctuation, preserve_intra_word_dashes = TRUE)
   
-  # Whitespace
+  ##### Whitespace
   crp <- tm_map(crp, stripWhitespace)
   
   # SAVE RESULTS
   save(crp, file = "crp.RDa")
 }
+
+#####################################################################
+##
+## Quanteda
+##
+#####################################################################
+
+Quan <- function() {
+  crp2 <- corpus(docs)
+  
+  #Quanteda tokens
+  crpT <- tokens(crp2, remove_punct = TRUE)
+  
+  #metadoc(crp2, "language") <- "english"
+  
+  ##### Clean unicode characters
+  ##### Remove graphical characters
+  crpT <- tokens_replace(crpT, "[^[:graph:]]", " ")
+  
+  ##### To lower
+  crpT <- tokens_tolower(crpT)
+  
+  ##### Stopword removal
+  crpT <- tokens_remove(crpT, stopwords(source = "smart"))
+  
+  ##### Stemming
+  crpT <- tokens_wordstem(crpT, language = "porter")
+  
+  ##### Numbers
+  
+  ##### All numbers (including numbers as part of a alphanumerical term) + punction + symbols
+  crpT <-
+    tokens(
+      crpT,
+      remove_numbers = TRUE,
+      remove_symbols = TRUE
+    )
+  
+  # SAVE RESULTS
+  save(crpT, file = "crpT.RDa")
+}
+
+
+microbenchmark(Quan(), VCorp(), times = 1)
+
--- DataOpdracht1/lib/readFiles.r
+++ DataOpdracht1/lib/readFiles.r
@@ -8,21 +8,9 @@ ifn <- "tls203_part"
 batches <- 5
 ifp <- "../../../data/mini/"
 ofn <- "ps18b_abstr"
-no_cores <- detectCores()
+core_no <- detectCores()
 
 # given a batch number, read batch and save in global env
-read_batch <- function(batch_nr) {
-  # Compile file name to read
-  batch_no <- sprintf("%02d", batch_nr)
-  fn <- paste0(ifn, batch_no, ".txt.xz")
-  fp <- ifp
-  #read batch file
-  print(paste0("Start reading batch <", batch_no, "> (", fn, ")"))
-  return(tibble::as_tibble(readr::read_delim(paste0(fp, fn), ",", col_names=TRUE, quote = "\"", 
-                                             escape_backslash = FALSE, escape_double = FALSE, na=character(), 
-                                             trim_ws= TRUE, skip=0, n_max=Inf, locale = locale(encoding = "UTF-8"))))
-}
-
 read_and_save_batch <- function(batch_nr) {
   # Compile file name to read
   batch_no <- sprintf("%02d", batch_nr)
@@ -32,13 +20,11 @@ read_and_save_batch <- function(batch_nr) {
   #read batch file
   print(paste0("Start reading batch <", batch_no, "> (", fn, ")"))
   v <- paste0(ofn, "_", batch_no)
-  assign(v,
-         tibble::as_tibble(readr::read_delim(paste0(fp, fn), ",", col_names=TRUE, quote = "\"", 
-                                             escape_backslash = FALSE, escape_double = FALSE, na=character(), 
-                                             trim_ws= TRUE, skip=0, n_max=Inf, locale = locale(encoding = "UTF-8")))
+  assign(v, tibble::as_data_frame(readr::read_delim(paste0(fp, fn), ",", col_names=TRUE, quote = "\"", 
+                                                    escape_backslash = FALSE, escape_double = FALSE, na=character(), 
+                                                    trim_ws= TRUE, skip=0, n_max=Inf, locale = locale(encoding = "UTF-8")))
          , envir = globalenv())
-  
-  # # Save intermediate batch file
+  # Save intermediate batch file
   fn <- paste0(ofn, "_", batch_no, ".xz.RDa")
   print(paste0("Start saving batch <", batch_no, "> to ", fn, "."))
   save(list=v, file=fn, compress="xz", compression_level=2)
@@ -46,62 +32,69 @@ read_and_save_batch <- function(batch_nr) {
 }
 
 makeReadFileCluster <- function() {
-  cl <- makeCluster(no_cores, outfile = "")
-  clusterExport(cl, c("read_batch", "ifn", "ifp", "ofn", "import"))
+  cl <- makeCluster(core_no, outfile = "")
+  clusterExport(cl, c("read_and_save_batch", "ifn", "ifp", "ofn", "import"))
   clusterEvalQ(cl, import(c("readr","tibble","data.table")))
   return(cl)
 }
 
-read_parlapply <- function() {
+read_and_save_parlapply <- function() {
   cl <- makeReadFileCluster()
-  res <- parLapply(cl, 1:batches, read_batch)
+  parLapply(cl, 1:batches, read_and_save_batch)
   stopCluster(cl)
-  return(as.data.frame(do.call(rbind, res)))
 }
 
-read_clusterapply <- function() {
+read_and_save_clusterapply <- function() {
   cl <- makeReadFileCluster()
-  res <- clusterApply(cl, 1:batches, read_batch)
+  clusterApply(cl, 1:batches, read_and_save_batch)
   stopCluster(cl)
-  str(res)
-  return(as.data.frame(do.call(rbind, res)))
 }
 
-read_doparallel_foreach <- function() {
+read_and_save_doparallel_foreach <- function() {
   cl <- makeReadFileCluster()
   registerDoParallel(cl)
-  res <- foreach(batch_nr = 1:batches, .combine = rbind) %dopar% {
-    read_batch(batch_nr)
+  
+  foreach(batch_nr = 1:batches, .combine = rbind) %dopar% {
+    read_and_save_batch(batch_nr)
   }
-  stopImplicitCluster()
-  return(res)
 }
 
-read_sequential <- function() {
-  res <- lapply(1:batches, read_batch)
-  return(as.data.frame(do.call(rbind, res)))
+read_and_save_sequential <- function() {
+  for(batch_nr in 1:batches) {
+    read_and_save_batch(batch_nr)
+  }
 }
 
 
 readFiles <- function(){
+  
   print("Read Files Process started.")
+  
   # READ AND PROCESS BATCHES
-  ps18b_abstr <- read_sequential()
+  
+  read_and_save_clusterapply()
+  
   # COMBINE BATCHES
-  # # Variable name to use
-  # w <- ofn
-  # # Pattern to use
-  # pt <- paste0(ofn, "_", "[0-9]+")
-  # print("Combining batches...")
-  # assign(w, as.data.frame(rbindlist(mget(ls(pattern=pt, envir = .GlobalEnv), envir = .GlobalEnv))))
+  
+  # Variable name to use
+  w <- ofn
+  # Pattern to use
+  pt <- paste0(ofn, "_", "[0-9]+")
+  print("Combining batches...")
+  assign(w, as.data.frame(rbindlist(mget(ls(pattern=pt, envir = globalenv()), envir = globalenv()))))
+  
+  
   # COMPILE DATAFRAME
+  
   print("Compiling to dataframe 'docs'.")
+  
   docs <- ps18b_abstr[,c("appln_id", "appln_abstract", "appln_abstract_lg")]
   names(docs) <- c("doc_id", "text", "language")
+  
   print("Finished reading batches.")
   return(docs)
 }
 
 benchmark_readFiles <- function() {
-  microbenchmark(read_clusterapply(), read_doparallel_foreach(), read_parlapply(), read_sequential(), times = 3)
+  microbenchmark(read_and_save_clusterapply(), read_and_save_doparallel_foreach(), read_and_save_parlapply(), read_and_save_sequential(), times = 3)
 }
