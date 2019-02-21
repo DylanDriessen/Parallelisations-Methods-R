3e167e11a8eb53d8a9c27f5e794bb7338d5ae1cb
Bjarne <bjarne.desmet@student.ucll.be>
2019-02-21 16:09:34 +0100

    Parallel in orde peakRAM in orde! Geeft de volledige tijd weer

--- DataOpdracht1/lib/readFiles.r
+++ DataOpdracht1/lib/readFiles.r
@@ -54,6 +54,14 @@ makeReadFileCluster <- function() {
   return(cl)
 }
 
+
+#PEAKRAM
+makeReadFileClusterPeakRAM <- function() {
+  cl <- makeReadFileCluster()
+  clusterEvalQ(cl, import("peakRAM"))
+  return(cl)
+}
+
 read_parlapply <- function() {
   cl <- makeReadFileCluster()
   res <- parLapply(cl, 1:batches, read_batch)
@@ -61,6 +69,20 @@ read_parlapply <- function() {
   res_df <- as.data.frame(do.call(rbind, res))
   rm(res)
   gc()
+  return(res_df)
+}
+
+#PEAKRAM
+read_parlapplyPeakRAM <- function() {
+  cl <- makeReadFileClusterPeakRAM()
+  res <- parLapply(cl, 1:batches, function(x) {
+    peakRAM(read_batch(x))[,2:4]
+  })
+  stopCluster(cl)
+  res_df <- as.data.frame(do.call(rbind, res))
+  rm(res)
+  gc()
+  res_df <- colSums(res_df)
   return(res_df)
 }
 
@@ -74,6 +96,24 @@ read_clusterapply <- function() {
   return(res_df)
 }
 
+#PEAKRAM
+read_clusterapply_peakRAM<- function() {
+  cl <- makeReadFileClusterPeakRAM()
+  res <- clusterApply(cl, 1:batches, function(x) {
+   peakRAM(read_batch(x))[,2:4]
+  })
+  stopCluster(cl)
+  res_df <- as.data.frame(do.call(rbind, res))
+  rm(res)
+  gc()
+  res_df <- colSums(res_df)
+  return(res_df)
+}
+
+
+
+
+
 read_doparallel_foreach <- function() {
   cl <- makeReadFileCluster()
   registerDoParallel(cl)
@@ -84,6 +124,22 @@ read_doparallel_foreach <- function() {
   gc()
   return(res)
 }
+
+#PEAKRAM
+read_doparallel_foreach_peakRAM <- function() {
+  cl <- makeReadFileClusterPeakRAM()
+  registerDoParallel(cl)
+  res <- foreach(batch_nr = 1:batches, .combine = rbind  ) %dopar% {
+    peakRAM(read_batch(batch_nr))[1,2:4]
+    
+  }
+  stopCluster(cl)
+  gc()
+  res <- colSums(res)
+  
+  return(res)
+}
+
 
 read_sequential <- function() {
   res <- lapply(1:batches, read_batch)
@@ -113,13 +169,18 @@ readFiles <- function(){
   return(docs)
 }
 
+
 #functie roept de 4 soorten functies op 
 call_functions_for_ram <- function(){
-    ram_data_functions <- data.frame(peakRAM( read_clusterapply(), read_doparallel_foreach(), read_parlapply(), read_sequential()))
-    ram_data <- rbind(ram_data_functions, ram_Seq_data)
+  ram_data <- rbind(parallelForeach = read_doparallel_foreach_peakRAM(), clusterApply = read_clusterapply_peakRAM(), parLapplyParallel = read_parlapplyPeakRAM())
+                    
+  saveRDS(ram_data, file = "~/R/Afstudeerwerk/DataOpdracht1/RShinyDashboardAfstudeer/data/ram_data.rds")
+    
+    
     saveRDS(ram_data, file = "~/R/Afstudeerwerk/DataOpdracht1/RShinyDashboardAfstudeer/data/ram_data.rds")
 }
- 
+
+
 benchmark_readFiles <- function() {
   microbenchmark(read_clusterapply(), read_doparallel_foreach(), read_parlapply(), read_sequential(), times = 1)
 }
