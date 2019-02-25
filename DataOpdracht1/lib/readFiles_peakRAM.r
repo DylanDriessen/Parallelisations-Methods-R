d8db99cd50c6dc9ca02a9a518473ef682794bb05
VincentHeeren <vincent.heeren@student.ucll.be>
2019-02-25 09:40:12 +0100

    ffdf + ram scripts

--- DataOpdracht1/lib/readFiles_peakRAM.r
+++ DataOpdracht1/lib/readFiles_peakRAM.r
@@ -20,20 +20,20 @@ read_doparallel_foreach_peakRAM <- function() {
   cl <- makeReadFileClusterPeakRAM()
   registerDoParallel(cl)
   on.exit(stopCluster(cl))
-  return(foreach(batch_nr = 1:batches, .combine = rbind  ) %dopar% read_batch_peakRAM(batch_nr))
+  return(add_process_index(foreach(batch_nr = 1:batches, .combine = rbind  ) %dopar% read_batch_peakRAM(batch_nr)))
 }
 read_clusterapply_peakRAM<- function() {
   cl <- makeReadFileClusterPeakRAM()
   on.exit(stopCluster(cl))
-  return(list_to_df(clusterApply(cl, 1:batches, read_batch_peakRAM)))
+  return(add_process_index(list_to_df(clusterApply(cl, 1:batches, read_batch_peakRAM))))
 }
 read_parlapply_peakRAM <- function() {
   cl <- makeReadFileClusterPeakRAM()
   on.exit(stopCluster(cl))
-  return(list_to_df(parLapply(cl, 1:batches, read_batch_peakRAM)))
+  return(add_process_index(list_to_df(parLapply(cl, 1:batches, read_batch_peakRAM))))
 }
 read_sequential_peakRAM <- function() {
-  return(list_to_df(lapply(1:batches, read_batch_peakRAM)))
+  return(add_process_index(list_to_df(lapply(1:batches, read_batch_peakRAM))))
 }
 
 #functie roept de 4 soorten functies op 
@@ -44,4 +44,9 @@ read_peakRAM_to_rds <- function(){
   saveRDS(read_parlapply_peakRAM(), file = paste0(sp, "read_parlapply_peakRAM.rds"))
   saveRDS(read_clusterapply_peakRAM(), file = paste0(sp, "read_clusterapply_peakRAM.rds"))
   saveRDS(read_sequential_peakRAM(), file = paste0(sp, "read_sequential_peakRAM.rds"))
+}
+
+add_process_index <- function(df) {
+  df$Core_No <- as.numeric(as.factor(df$Process_Id))
+  return(df)
 }
