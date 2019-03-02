90330f44c74f0e57228e7ef441b654975daa32b1
Vincent Heeren <vincent.heeren@student.ucll.be>
2019-03-02 15:31:16 +0100

    fix save function data

--- DataOpdracht1/lib/readFiles_peakRAM.r
+++ DataOpdracht1/lib/readFiles_peakRAM.r
@@ -16,6 +16,19 @@ read_batch_peakRAM <- function(x) {
 }
 
 ### LOOPS
+# read_doparallel_foreach_peakRAM2 <- function() {
+#   png(paste0('test', '/snow_plot.png'))
+#   plot(snow.time({
+#     cl <- makeCluster(min(no_cores, batches), outfile = "")
+#     clusterExport(cl, c("read_batch", "read_batch_peakRAM", "ifn", "ifp", "ofn"))
+#     clusterEvalQ(cl, { library("readr"); library("tibble"); library("data.table"); library("peakRAM") })
+#     registerDoSNOW(cl)
+#     res <- foreach(batch_nr = 1:batches, .combine = rbind  ) %dopar% read_batch_peakRAM(batch_nr)
+#     stopCluster(cl)}
+#   ))
+#   dev.off()
+#   return(res)
+# }
 read_doparallel_foreach_peakRAM <- function() {
   cl <- makeReadFileClusterPeakRAM()
   registerDoSNOW(cl)
