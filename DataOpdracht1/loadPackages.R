abfebdaaa5f079fd17ba1c0df090653d77e69cbe
Dylan <dylan.driessen@student.ucll.be>
2019-03-09 10:19:39 +0100

    commit

--- DataOpdracht1/loadPackages.R
+++ DataOpdracht1/loadPackages.R
@@ -3,4 +3,4 @@ import(c("readr","tibble","data.table", "parallel", "foreach", "doSNOW", "snow",
          "stringi", "ff", "ffbase", "tm","SnowballC","slam","stringi","data.table",
          "magrittr","corrplot","NLP", "foreach","doParallel","microbenchmark",
          "text2vec","doMC","quanteda","textmineR", "parallel", "peakRAM",
-         "microbenchmark","glmnet", "quanteda", "tcltk2", "pryr", "dplyr", "stopwords", "skmeans"))
+         "microbenchmark","glmnet", "quanteda", "tcltk2", "pryr", "dplyr", "stopwords", "skmeans","biganalytics", "cluster"))
